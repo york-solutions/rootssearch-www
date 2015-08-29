0962365e85603e0444b5af91edb2f7b44bdf3862
Justin <justincyork@gmail.com>
2015-08-29 15:43:05 +0000

    save settings

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,34 +1,33 @@
+var sitesList;
+
 $(function(){
-  
   Settings.load();
   SearchSite.template = Handlebars.compile($('#sites-template').html());
-  
-  var availableSites = gensites.sites(),
-      enabledSites = Settings.get('sites'),
-      $sitesList = $('#sites-list');
-  for(var i = 0; i < availableSites.length; i++){
-    var gensite = availableSites[i],
-        searchSite = new SearchSite(gensite);
-    $sitesList.append(searchSite.getDOM());
-    if(enabledSites.indexOf(gensite.id) !== -1){
-      searchSite.enable();
-    } else {
-      searchSite.disable();
-    }
-  }
+  sitesList = new SitesList('#sites-list');
 });
 
 /**
  * Manage a site's display and settings
  */
-var SearchSite = function(site){
-  this._site = site;
-  this.render();
+var SearchSite = function(site, enabled){
+  this.site = site;
+  this.enabled = enabled;
+  this.changeFunc = function(){};
 };
 
 SearchSite.prototype.render = function(){
   var self = this;
-  self.$dom = $(SearchSite.template(self._site));
+  self.$dom = $(SearchSite.template({
+    site: self.site,
+    enabled: self.enabled
+  }));
+  self.$dom.find('.enabled-btn').click(function(){
+    self.enable();
+  });
+  self.$dom.find('.disabled-btn').click(function(){
+    self.disable();
+  });
+  return this;
 };
 
 SearchSite.prototype.getDOM = function(){
@@ -36,22 +35,72 @@ SearchSite.prototype.getDOM = function(){
 };
 
 SearchSite.prototype.isEnabled = function(){
-  return !this.$dom.hasClass('disabled');
+  return this.enabled;
 };
 
 SearchSite.prototype.disable = function(){
-  this.$dom.addClass('disabled');
-  this.update();
+  if(this.enabled){
+    this.enabled = false;
+    this.changed();
+  }
+  return this;
 };
 
 SearchSite.prototype.enable = function(){
-  this.$dom.removeClass('disabled');
-  this.update();
+  if(!this.enabled){
+    this.enabled = true;
+    this.changed();
+  }
+  return this;
 };
 
-SearchSite.prototype.update = function(){
-  var val = this.isEnabled() ? 'enabled' : 'disabled';
-  this.$dom.find('.' + val + '-btn').click();
+SearchSite.prototype.changed = function(){
+  var self = this;
+  setTimeout(function(){
+    self.changeFunc();
+  });
+};
+
+SearchSite.prototype.onChange = function(func){
+  this.changeFunc = func;
+};
+
+/**
+ * Manage the display of the list of site
+ */
+var SitesList = function(selector){
+  this.$container = $(selector);
+  this.sites = [];
+  
+  var availableSites = gensites.sites(),
+      enabledSites = Settings.get('sites');
+  for(var i = 0; i < availableSites.length; i++){
+    var gensite = availableSites[i],
+        enabled = enabledSites.indexOf(gensite.id) !== -1,
+        searchSite = new SearchSite(gensite, enabled);
+    this.addSite(searchSite);
+  }
+};
+
+SitesList.prototype.addSite = function(site){
+  var self = this;
+  self.sites.push(site);
+  self.$container.append(site.render().getDOM());
+  site.onChange(function(){
+    self.updateSettings();
+  });
+};
+
+SitesList.prototype.updateSettings = function(){
+  var enabledSites = [];
+  for(var i = 0; i < this.sites.length; i++){
+    if(this.sites[i].isEnabled()){
+      enabledSites.push(this.sites[i].site.id);
+    }
+  }
+  console.log(enabledSites);
+  Settings.set('sites', enabledSites);
+  Settings.save();
 };
 
 /**
@@ -72,7 +121,13 @@ var Settings = {
     }
     _.defaultsDeep(this._settings, parsed, this._defaults);
   },
+  save: function(){
+    cookies.setItem('settings', JSON.stringify(this._settings));
+  },
   get: function(key){
     return this._settings[key];
+  },
+  set: function(key, value){
+    this._settings[key] = value;
   }
 };
