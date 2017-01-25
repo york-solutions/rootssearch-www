be927fa0fdc02849d12fd1737acf3616e5453355
Justin <justincyork@gmail.com>
2017-01-25 22:59:15 +0000

    accept gedcomx

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ var favicon = require('serve-favicon');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(require('./middleware/locals'));
 app.use(favicon(__dirname + '/assets/favicon.ico'));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(require('./middleware/locals'));
+app.use(bodyParser.urlencoded({ 
+  extended: true,
+  limit: '500kb'
+}));
 
 // Routes
 app.use('/', require('./routes/index'));
@@ -18,9 +21,15 @@ app.use('/search', require('./routes/search'));
 app.use('/settings', require('./routes/settings'));
 app.use('/help', require('./routes/help'));
 
-// Catch 404s
+// 404 if we couldn't match a route.
 app.use(function(req, res, next) {
   res.status(404).render('404');
+});
+
+// Catch errors
+app.use(function(error, req, res, next){
+  console.error(error.stack);
+  res.status(500).render('500');
 });
 
 var server = app.listen(process.env.PORT, process.env.IP, function () {
--- routes/search.js
+++ routes/search.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var gedx = require('../lib/gedx.js');
+var debug = require('debug')('rootssearch:routes:search');
 
 router.get('/', function(req, res){
   res.render('search', {
@@ -8,16 +10,39 @@ router.get('/', function(req, res){
   });
 });
 
+/**
+ * Data has been POSTed for pre-filling the search form.
+ */
 router.post('/', function(req, res){
   var data = {};
+  
+  // Do we actually have data?
   if(req.body) {
+    
+    // If GedcomX, convert to gensearch v1 schema. Otherwise, assume it is 
+    // already gensearch v1 schema.
     if(req.body.data){
+      debug('gensearch');
       data = req.body.data;
     }
+    
+    else if(req.body.gedcomx){
+      debug('gedcomx');
+      data = gedx.convertToGensearch(JSON.parse(req.body.gedcomx));
+    }
+    
+    // Before the rootssearch.io website was created, the extension handled
+    // site settings and searching. When the extension is upgraded to v4 and
+    // POSTs for the first time then it sends those site settings. We still
+    // have a few hundred users on v3. Even when all users have migrated to
+    // v4 we don't yet have a way of tracking whether they've sent a POST yet.
+    // TODO: add tracking to know when we can remove this. Or decide that it's
+    // not worth keeping around; users can change the site settings themselves.
     if(req.body._sites){
       res.cookie('settings', JSON.stringify({sites: req.body._sites.split(',')}));
     }
   }
+  
   res.render('search', {
     data: data
   });
