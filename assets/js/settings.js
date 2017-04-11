b6c8486de50f58cfa0d85a31fc898be1217e3b5e
Justin <justincyork@gmail.com>
2017-04-11 21:31:49 +0000

    refactor

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -2,8 +2,7 @@ var Settings = require('./settings-manager'),
     gensites = require('gensites'),
     React = require('react'),
     ReactDOM = require('react-dom'),
-    Redux = require('redux'),
-    $ = require('jquery');
+    Redux = require('redux');
 
 /**
  * Manage the display of the list of sites
@@ -50,131 +49,62 @@ class SearchSite extends React.Component {
   }
 }
 
-document.addEventListener("DOMContentLoaded", function(){
+function sitesReducer(sitesState = [], action){
+  let enable = true;
+  switch(action.type){
+    // Toggle site. Take advantage of switch statement fallthrough
+    // to reuse code for toggling a site.
+    case 'DISABLE_SITE':
+      enable = false;
+    case 'ENABLE_SITE':
+      return sitesState.map(site => {
+        return Object.assign({}, site, {
+          enabled: site.id === action.site ? enable : site.enabled
+        });
+      });
+    default:
+      return sitesState;
+  }
+}
+
+function loadSiteSettings(){
   Settings.load();
   const sites = gensites.sites(),
       enabledSites = Settings.get('sites');
   sites.forEach(site => {
     site.enabled = enabledSites.indexOf(site.id) !== -1;
   });
-  
-  const store = Redux.createStore((state = [], action) => {
-    switch(action.type){
-      case 'ENABLE_SITE':
-      case 'DISABLE_SITE':
-      default:
-        return state;
-    }
-  }, sites);
-  
-  const enableSite = (siteId) => {
-    console.log('enable site', siteId);
-    store.dispatch({
-      type: 'ENABLE_SITE',
-      site: siteId
-    });
-  };
-  const disableSite = (siteId) => {
-    console.log('disable site', siteId);
-    store.dispatch({
-      type: 'DISABLE_SITE',
-      site: siteId
-    });
-  };
-  
+  return sites;
+}
+
+function init(){
+  const sites = loadSiteSettings();
+  const store = Redux.createStore((state, action) => {
+    return {
+      sites: sitesReducer(state.sites, action)
+    };
+  }, { sites });
   const render = () => ReactDOM.render(
-    <SitesList sites={store.getState()} enableSite={enableSite} disableSite={disableSite} />, 
+    <SitesList 
+      sites={store.getState().sites} 
+      enableSite={(siteId) => {
+        console.log('enable site', siteId);
+        store.dispatch({
+          type: 'ENABLE_SITE',
+          site: siteId
+        });
+      }} 
+      disableSite={(siteId) => {
+        console.log('disable site', siteId);
+        store.dispatch({
+          type: 'DISABLE_SITE',
+          site: siteId
+        });
+      }} />, 
     document.getElementById('sites-list')
   );
-  
   store.subscribe(render);
   render();
-});
+}
 
-/**
- * Manage a site's display and settings
- */
- /*
-var SearchSite = function(site, enabled){
-  this.site = site;
-  this.enabled = enabled;
-  this.changeFunc = function(){};
-};
-
-SearchSite.prototype.render = function(){
-  var self = this;
-  self.$dom = $(`<div class="site row">
-    <div class="site-description col-md-10">
-      <div class="site-name">${this.site.name}</div>
-      <p class="text-muted">${this.site.description.en}</p>
-    </div>
-    <div class="site-settings-col col-md-2">
-      <div class="btn-group btn-toggle" data-toggle="buttons">
-        <label class="btn btn-rs enabled-btn">
-          <input type="radio" name="enabled" autocomplete="off" value="enabled"> Enabled
-        </label>
-        <label class="btn btn-rs disabled-btn">
-          <input type="radio" name="enabled" autocomplete="off" value="disabled"> Disabled
-        </label>
-      </div>
-    </div>
-    <div class="col-xs-12"><hr></div>
-  </div>`);
-  self.updateButtonStates();
-  self.$dom.find('.enabled-btn').click(function(){
-    self.enable();
-  });
-  self.$dom.find('.disabled-btn').click(function(){
-    self.disable();
-  });
-  return this;
-};
-
-SearchSite.prototype.getDOM = function(){
-  return this.$dom;
-};
-
-SearchSite.prototype.isEnabled = function(){
-  return this.enabled;
-};
-
-SearchSite.prototype.disable = function(){
-  if(this.enabled){
-    this.enabled = false;
-    this.changed();
-    ga('send', 'event', 'settings', 'disableSite', this.site.id);
-  }
-  return this;
-};
-
-SearchSite.prototype.enable = function(){
-  if(!this.enabled){
-    this.enabled = true;
-    this.changed();
-    ga('send', 'event', 'settings', 'enableSite', this.site.id);
-  }
-  return this;
-};
-
-SearchSite.prototype.updateButtonStates = function(){
-  if(this.enabled){
-    this.$dom.find('.enabled-btn').addClass('active');
-    this.$dom.find('.disabled-btn').removeClass('active');
-  } else {
-    this.$dom.find('.disabled-btn').addClass('active');
-    this.$dom.find('.enabled-btn').removeClass('active');
-  }
-};
-
-SearchSite.prototype.changed = function(){
-  var self = this;
-  self.updateButtonStates();
-  setTimeout(function(){
-    self.changeFunc();
-  });
-};
-
-SearchSite.prototype.onChange = function(func){
-  this.changeFunc = func;
-};
-*/
+document.addEventListener("DOMContentLoaded", init);
