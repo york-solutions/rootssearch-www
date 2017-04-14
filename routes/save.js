5698c9affd8301810c930db05ead1c5504ebeb04
Justin <justincyork@gmail.com>
2017-04-14 18:12:29 +0000

    configure POST route

--- routes/save.js
+++ routes/save.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/:site', function(req, res) {
-  res.render(`save/${req.params.site}`);
+router.post('/:site', function(req, res) {
+  if(req.body && req.body.gedcomx){
+    res.render(`save/${req.params.site}`, {
+      gedcomx: req.body.gedcomx
+    });
+  } else {
+    res.status(400).send('Missing GEDCOM X data');
+  }
 });
+
+if(process.env.NODE_ENV !== 'production'){
+  router.get('/fs/post', function(req, res){
+    res.render('save/fsPost');
+  });
+}
 
 module.exports = router;
