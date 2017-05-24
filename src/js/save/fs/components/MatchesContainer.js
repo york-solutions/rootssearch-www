547bd1773d4f2365303e41c9d8ab68e1f3ef91c6
Justin <justincyork@gmail.com>
2017-05-24 21:32:37 +0000

    single flat MatchesContainer not working well with selectors because some data isn't available

--- src/js/save/fs/components/MatchesContainer.js
+++ src/js/save/fs/components/MatchesContainer.js
@@ -5,12 +5,14 @@ const Fact = require('./Fact');
 const Name = require('./Name');
 const Family = require('./Family');
 const MatchesList = require('./MatchesList');
-const SelectedMatch = require('./SelectedMatch');
 
 const matchedSelector = require('../selectors/matched');
 const savedSelector = require('../selectors/saved');
 const currentPersonSelector = require('../selectors/currentPerson');
 const sourceDescriptionSelector = require('../selectors/sourceDescription');
+const updatedNamePartsSelector = require('../selectors/updatedNameParts');
+const matchFactsSelector = require('../selectors/updatedMatchFacts');
+const nameModifiedSelector = require('../selectors/nameModified');
 
 class MatchesContainer extends React.Component {
   
@@ -22,6 +24,26 @@ class MatchesContainer extends React.Component {
   }
   
   render(){
+    const { matched = false } = this.props;
+
+    let rows = [
+      ...this.renderRecordPerson()
+    ];
+    
+    if(matched){
+      rows.push(...this.renderSelectedMatch());
+    } else {
+      rows.push(<MatchesList />);
+    }
+  
+    return (
+      <div className="matches-grid">
+        {rows}
+      </div>
+    );
+  }
+  
+  renderRecordPerson(){
     const { 
         person, 
         record, 
@@ -35,46 +57,129 @@ class MatchesContainer extends React.Component {
         sourceDescription
       } = this.props,
       copyable = matched && !saved;
-      
-    return (
-      <div className="matches-grid">
-      
-        {/* Begin left column (record person) */}
-        <div className="label">Record Person</div>
-        <BoxTitle>
-          <div className="person-name large">{person.getDisplayName(true)}</div>
-          <div className="record-title">{sourceDescription.getTitles()[0].getValue()}</div>
-        </BoxTitle>
-        <div className="box-row">
-          <Name 
-            name={person.getPreferredName()} 
-            copied={nameCopied} 
-            copyable={copyable} 
-            onCopyChange={this.onNameCopyChange} />
-        </div>
-        {factOrder.map(id => {
-          return (
-            <div className="box-row" key={id}>
-              <Fact
-                fact={facts[id]} 
-                copyable={copyable}
-                dateCopied={!!copiedDates[id]}
-                placeCopied={!!copiedPlaces[id]}
-                onDateCopyChange={this.onDateCopyChange} 
-                onPlaceCopyChange={this.onPlaceCopyChange} />
-            </div>
-          );
-        })}
-        <div className="box-end"></div>
-        <Family gedcomx={record} personId={person.getId()} />
-        {/* End left column */}
-        
-        {/* Begin right column (matches) */}
-        { matched ? <SelectedMatch /> : <MatchesList /> }
-        {/* End right column (matches) */}
-        
-      </div>
-    );
+    return [<div className="label">Record Person</div>,
+      <BoxTitle>
+        <div className="person-name large">{person.getDisplayName(true)}</div>
+        <div className="record-title">{sourceDescription.getTitles()[0].getValue()}</div>
+      </BoxTitle>,
+      <div className="box-row">
+        <Name 
+          name={person.getPreferredName()} 
+          copied={nameCopied} 
+          copyable={copyable} 
+          onCopyChange={this.onNameCopyChange} />
+      </div>,
+      ...(factOrder.map(id => {
+        return (
+          <div className="box-row">
+            <Fact
+              fact={facts[id]} 
+              copyable={copyable}
+              dateCopied={!!copiedDates[id]}
+              placeCopied={!!copiedPlaces[id]}
+              onDateCopyChange={this.onDateCopyChange} 
+              onPlaceCopyChange={this.onPlaceCopyChange} />
+          </div>
+        );
+      })),
+      <div className="box-end"></div>,
+      <Family gedcomx={record} personId={person.getId()} />];
+  }
+  
+  renderSelectedMatch(){
+    const {
+      matchPerson,
+      saved,
+      nameParts,
+      nameReason,
+      nameModified,
+      matchFacts,
+      dispatch,
+      matchId,
+      gedcomx
+    } = this.props;
+    return [
+      <div className="label">Tree Person</div>,
+      <PersonBoxTitle person={matchPerson} />,
+      saved ? 
+        <Name name={matchPerson.getPreferredName()} /> :
+        <EditableName   
+          nameParts={nameParts} 
+          attribution={matchPerson.getPreferredName().getAttribution()}
+          reason={nameReason}
+          modified={nameModified}
+          onChange={this.nameChangeHandler.bind(this)} />,
+      ...matchFacts.map(({fact, display, modified, originalAttribution}) => {
+        return (
+          <div key={fact.getId()}>
+            <hr />
+            { this.props.saved ?
+              <Fact fact={fact} /> :
+              (display ?
+                <EditableFact 
+                  fact={fact}
+                  modified={modified}
+                  originalAttribution={originalAttribution}
+                  onDateChange={this.dateChangeHandler(fact.getId()).bind(this)} 
+                  onPlaceChange={this.placeChangeHandler(fact.getId()).bind(this)} /> :
+                <div className="fact-placeholder" />
+              )
+            }
+          </div>
+        );
+      }),
+      saved ? 
+        <button className="btn btn-lg disabled" disabled>Saved</button> :
+        (
+          <div className="toolbar">
+            <button className="btn btn-orange btn-lg" onClick={() => dispatch({ type: 'REVIEW_UPDATES' })}>Save</button>
+            <a href onClick={this.cancelMatch.bind(this)}>Cancel</a>
+          </div>
+        ),
+      <Family gedcomx={gedcomx} personId={matchId} />
+    ];
+  }
+  
+  nameChangeHandler(type){
+    return (event) => {
+      this.props.dispatch({
+        type: 'OVERRIDE_NAMEPART',
+        value: event.target.value,
+        partType: type,
+        personId: this.props.personId
+      });
+    };
+  }
+  
+  dateChangeHandler(factId){
+    return (date) => {
+      this.props.dispatch({
+        type: 'OVERRIDE_DATE',
+        value: date,
+        dataId: factId,
+        personId: this.props.personId
+      });
+    };
+  }
+  
+  placeChangeHandler(factId){
+    return (place) => {
+      this.props.dispatch({
+        type: 'OVERRIDE_PLACE',
+        value: place,
+        dataId: factId,
+        personId: this.props.personId
+      });
+    };
+  }
+  
+  cancelMatch(e) {
+    this.props.dispatch({
+      type: 'CANCEL_MATCH',
+      personId: this.props.personId
+    });
+    e.preventDefault(); 
+    return false;
   }
   
   onCopyChange(type){
@@ -92,7 +197,8 @@ class MatchesContainer extends React.Component {
 
 const mapStateToProps = state => {
   const currentPerson = currentPersonSelector(state),
-        match = currentPerson.selectedMatch;
+        match = currentPerson.selectedMatch,
+        {matchId, gedcomx, copyName, nameReason} = match;
   return {
     record: state.record,
     person: currentPerson.gedcomx,
@@ -104,7 +210,15 @@ const mapStateToProps = state => {
     nameCopied: match.copyName,
     copiedDates: match.copiedDates,
     copiedPlaces: match.copiedPlaces,
-    sourceDescription: sourceDescriptionSelector(state)
+    sourceDescription: sourceDescriptionSelector(state),
+    matchId,
+    matchPerson: gedcomx ? gedcomx.getPersonById(matchId) : null,
+    gedcomx,
+    copyName,
+    nameReason,
+    nameModified: nameModifiedSelector(state),
+    nameParts: updatedNamePartsSelector(state),
+    matchFacts: matchFactsSelector(state)
   };
 };
 
