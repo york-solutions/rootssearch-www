a1f406a14120986daf2948ad354893a72a2f38e3
Justin <justincyork@gmail.com>
2017-09-07 14:53:18 +0000

    source maps

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,9 @@ const config = {
 };
 
 if(process.env.NODE_ENV === 'production'){
-  config.plugins.push(new webpack.optimize.UglifyJsPlugin());
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+    sourceMap: true
+  }));
   config.plugins.push(new webpack.DefinePlugin({
     'process.env.NODE_ENV': JSON.stringify('production')
   }));
